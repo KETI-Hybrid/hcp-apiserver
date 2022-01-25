f649ef72d4f215c259cfb62b4cef076021a18840
lcw729 <lcw729729@gmail.com>
2022-01-25 16:50:43 +0900

    aks addon CLI

---
+++ pkg/util/output.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os/exec"
+)
+
+type Output struct {
+	Stderr []byte
+	Stdout []byte
+}
+
+func CombinedOutput2(cmd *exec.Cmd) ([]byte, []byte) {
+	stdout, err := cmd.StdoutPipe()
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	errb, _ := io.ReadAll(stderr)
+	outb, _ := io.ReadAll(stdout)
+	cmd.Wait()
+	return errb, outb
+}
+
+func GetOutput(cmd *exec.Cmd) ([]byte, error) {
+	errb, outb := CombinedOutput2(cmd)
+	output := Output{
+		Stderr: errb,
+		Stdout: outb,
+	}
+	data, err := json.Marshal(output)
+	return data, err
+}
