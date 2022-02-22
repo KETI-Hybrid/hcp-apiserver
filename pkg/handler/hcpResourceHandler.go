f3bf0897baf8d98da4e38709c3240917e7e7854c
lcw729 <lcw729729@gmail.com>
2022-02-22 15:30:11 +0900

    create hcpdeployment if target cluster is not specified

--- pkg/handler/hcpResourceHandler.go
+++ pkg/handler/hcpResourceHandler.go
@@ -2,11 +2,15 @@ package handler
 
 import (
 	cobrautil "Hybrid_Cluster/hybridctl/util"
+	resourcev1alpha1 "Hybrid_Cluster/pkg/apis/resource/v1alpha1"
+	resourcev1alpha1clientset "Hybrid_Cluster/pkg/client/resource/v1alpha1/clientset/versioned"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	policy "Hybrid_Cluster/resource"
 
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -34,9 +38,45 @@ func CreateDeploymentHandler(w http.ResponseWriter, r *http.Request) {
 	bytes, _ := json.Marshal(resource.RealResource)
 	json.Unmarshal(bytes, &real_resource)
 
+	algorithm, err := policy.GetAlgorithm()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	if resource.TargetCluster == "undefined" {
+
+		master_config, err := cobrautil.BuildConfigFromFlags("kube-master", "/root/.kube/config")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		clienset, err := resourcev1alpha1clientset.NewForConfig(master_config)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 		// analytic Engine
-		fmt.Println("scoring target_cluster")
+		hcp_resource := resourcev1alpha1.HCPDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: real_resource.Name,
+			},
+			Spec: resourcev1alpha1.HCPDeploymentSpec{
+				RealDeploymentSpec:     real_resource.Spec,
+				RealDeploymentMetadata: real_resource.ObjectMeta,
+
+				SchedulingStatus: "Requested",
+				SchedulingType:   algorithm,
+			},
+		}
+
+		r, err := clienset.HcpV1alpha1().HCPDeployments("hcp").Create(context.TODO(), &hcp_resource, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Printf("[1] Request scheduling to scheduler : %s \n", r.Name)
+		}
 	} else {
 		config, err := cobrautil.BuildConfigFromFlags(resource.TargetCluster, "/root/.kube/config")
 		if err != nil {
@@ -57,7 +97,29 @@ func CreateDeploymentHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(err)
 			return
 		} else {
-			fmt.Printf("success to create hcp_resource %s \n", r.Name)
+			fmt.Printf("success to create deployment %s \n", r.Name)
 		}
+	}
+}
+
+func DeleteDeploymentHandler(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	target_cluster := vals.Get("cluster")
+	namespace := vals.Get("namespace")
+	name := vals.Get("name")
+	config, err := cobrautil.BuildConfigFromFlags(target_cluster, "/root/.kube/config")
+	if err != nil {
+		fmt.Println(err)
+	}
+	clientset, _ := kubernetes.NewForConfig(config)
+
+	// create resource
+	err = clientset.AppsV1().Deployments(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Printf("success to delete deployment %s \n", name)
 	}
 }
