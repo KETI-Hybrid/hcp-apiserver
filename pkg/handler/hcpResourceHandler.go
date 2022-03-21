bcfc3bedb3ecde1a80c46ba83fbea2c4b3c59f57
lcw729 <lcw729729@gmail.com>
2022-03-21 14:56:22 +0900

    Create HCPPod CRD & hybridctl create CLI

--- pkg/handler/hcpResourceHandler.go
+++ pkg/handler/hcpResourceHandler.go
@@ -13,6 +13,7 @@ import (
 	policy "Hybrid_Cloud/hcp-resource/hcppolicy"
 
 	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -134,5 +135,100 @@ func DeleteDeploymentHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	} else {
 		fmt.Printf("success to delete deployment %s \n", name)
+	}
+}
+
+func CreatePodHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("111")
+	var resource Resource
+
+	jsonDataFromHttp, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(jsonDataFromHttp, &resource)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	w.Header().Set("Content-Type", "application/json")
+
+	// RealResource 읽어오기
+	var real_resource *v1.Pod
+	bytes, _ := json.Marshal(resource.RealResource)
+	json.Unmarshal(bytes, &real_resource)
+
+	// HCPPolicy 최적 배치 알고리즘 정책 읽어오기
+	algorithm, err := policy.GetAlgorithm()
+	fmt.Println(algorithm)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(resource.TargetCluster)
+	// TargetCluster가 지정되지 않은 경우
+	if resource.TargetCluster == "undefined" {
+
+		master_config, err := cobrautil.BuildConfigFromFlags("kube-master", "/root/.kube/config")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		clienset, err := resourcev1alpha1clientset.NewForConfig(master_config)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// HCPDeployment 생성하기
+		hcp_resource := resourcev1alpha1.HCPPod{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "HCPPod",
+				APIVersion: "hcp.crd.com/v1alpha1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: real_resource.Name,
+			},
+			Spec: resourcev1alpha1.HCPPodSpec{
+				RealPodSpec:     real_resource.Spec,
+				RealPodMetadata: real_resource.ObjectMeta,
+
+				// SchedulingStatus "Requested"
+				SchedulingStatus: "Requested",
+				SchedulingType:   algorithm,
+			},
+		}
+
+		r, err := clienset.HcpV1alpha1().HCPPods("hcp").Create(context.TODO(), &hcp_resource, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Printf("request scheduling to scheduler : %s \n", r.Name)
+		}
+	} else {
+		// TargetCluster가 지정된 경우
+		config, err := cobrautil.BuildConfigFromFlags(resource.TargetCluster, "/root/.kube/config")
+		if err != nil {
+			fmt.Println(err)
+		}
+		clientset, _ := kubernetes.NewForConfig(config)
+
+		// namespace
+		namespace := real_resource.ObjectMeta.Namespace
+		if namespace == "" {
+			namespace = "default"
+		}
+
+		// Kubernetes Deployment 생성
+		r, err := clientset.CoreV1().Pods(namespace).Create(context.TODO(), real_resource, metav1.CreateOptions{})
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Printf("success to create pod %s \n", r.Name)
+		}
 	}
 }
