1ccbe22b328ee2615a74f2c006580dfd580569a6
lcw729 <lcw729729@gmail.com>
2022-03-21 14:56:22 +0900

    Create HCPPod CRD & hybridctl create CLI

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -3,13 +3,14 @@ package main
 import (
 
 	// "Hybrid_Cloud/hybridctl/util"
-
 	"Hybrid_Cloud/hcp-apiserver/pkg/handler"
 	aksFunc "Hybrid_Cloud/hcp-apiserver/pkg/main/aks"
 	eksFunc "Hybrid_Cloud/hcp-apiserver/pkg/main/eks"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func CheckErr(err error) {
@@ -18,102 +19,105 @@ func CheckErr(err error) {
 	}
 }
 
-func AKSHandler() {
+func handlerRequests() http.Handler {
+
+	mux := mux.NewRouter()
 	// aks
 
 	// addon
-	http.HandleFunc("/addonDisable", aksFunc.AddonDisable)
-	http.HandleFunc("/addonEnable", aksFunc.AddonEnable)
-	http.HandleFunc("/addonList", aksFunc.AddonList)
-	http.HandleFunc("/addonListAvailable", aksFunc.AddonListAvailable)
-	http.HandleFunc("/addonShow", aksFunc.AddonShow)
-	http.HandleFunc("/addonUpdate", aksFunc.AddonUpdate)
+	mux.HandleFunc("/addonDisable", aksFunc.AddonDisable)
+	mux.HandleFunc("/addonEnable", aksFunc.AddonEnable)
+	mux.HandleFunc("/addonList", aksFunc.AddonList)
+	mux.HandleFunc("/addonListAvailable", aksFunc.AddonListAvailable)
+	mux.HandleFunc("/addonShow", aksFunc.AddonShow)
+	mux.HandleFunc("/addonUpdate", aksFunc.AddonUpdate)
 
 	// pod-identity
-	http.HandleFunc("/podIdentityAdd", aksFunc.PodIdentityAdd)
-	http.HandleFunc("/podIdentityDelete", aksFunc.PodIdentityDelete)
-	http.HandleFunc("/podIdentityList", aksFunc.PodIdentityList)
-	http.HandleFunc("/podIdentityExceptionAdd", aksFunc.PodIdentityExceptionAdd)
-	http.HandleFunc("/podIdentityExceptionDelete", aksFunc.PodIdentityExceptionDelete)
-	http.HandleFunc("/podIdentityExceptionList", aksFunc.PodIdentityExceptionList)
-	http.HandleFunc("/podIdentityExceptionUpdate", aksFunc.PodIdentityExceptionUpdate)
+	mux.HandleFunc("/podIdentityAdd", aksFunc.PodIdentityAdd)
+	mux.HandleFunc("/podIdentityDelete", aksFunc.PodIdentityDelete)
+	mux.HandleFunc("/podIdentityList", aksFunc.PodIdentityList)
+	mux.HandleFunc("/podIdentityExceptionAdd", aksFunc.PodIdentityExceptionAdd)
+	mux.HandleFunc("/podIdentityExceptionDelete", aksFunc.PodIdentityExceptionDelete)
+	mux.HandleFunc("/podIdentityExceptionList", aksFunc.PodIdentityExceptionList)
+	mux.HandleFunc("/podIdentityExceptionUpdate", aksFunc.PodIdentityExceptionUpdate)
 
 	// maintenanceconfiguration
-	http.HandleFunc("/maintenanceconfigurationCreateOrUpdate", aksFunc.MaintenanceconfigurationCreateOrUpdate)
-	http.HandleFunc("/maintenanceconfigurationDelete", aksFunc.MaintenanceconfigurationDelete)
-	http.HandleFunc("/maintenanceconfigurationList", aksFunc.MaintenanceconfigurationList)
-	http.HandleFunc("/maintenanceconfigurationShow", aksFunc.MaintenanceconfigurationShow)
+	mux.HandleFunc("/maintenanceconfigurationCreateOrUpdate", aksFunc.MaintenanceconfigurationCreateOrUpdate)
+	mux.HandleFunc("/maintenanceconfigurationDelete", aksFunc.MaintenanceconfigurationDelete)
+	mux.HandleFunc("/maintenanceconfigurationList", aksFunc.MaintenanceconfigurationList)
+	mux.HandleFunc("/maintenanceconfigurationShow", aksFunc.MaintenanceconfigurationShow)
 
 	// k8sconfiguration
-	http.HandleFunc("/configurationCreate", aksFunc.ConfigurationCreate)
-	http.HandleFunc("/configurationDelete", aksFunc.ConfigurationDelete)
-	http.HandleFunc("/configurationList", aksFunc.ConfigurationList)
-	http.HandleFunc("/configurationShow", aksFunc.ConfigurationShow)
+	mux.HandleFunc("/configurationCreate", aksFunc.ConfigurationCreate)
+	mux.HandleFunc("/configurationDelete", aksFunc.ConfigurationDelete)
+	mux.HandleFunc("/configurationList", aksFunc.ConfigurationList)
+	mux.HandleFunc("/configurationShow", aksFunc.ConfigurationShow)
 
 	// connectedk8s
-	http.HandleFunc("/connectedk8sConnect", aksFunc.Connectedk8sConnect)
-	http.HandleFunc("/connectedk8sDelete", aksFunc.Connectedk8sDelete)
-	http.HandleFunc("/connectedk8sDisableFeatures", aksFunc.Connectedk8sDisableFeatures)
-	http.HandleFunc("/connectedk8sEnableFeatures", aksFunc.Connectedk8sEnableFeatures)
-	http.HandleFunc("/connectedk8sList", aksFunc.Connectedk8sList)
-	http.HandleFunc("/connectedk8sProxy", aksFunc.Connectedk8sProxy)
-	http.HandleFunc("/connectedk8sShow", aksFunc.Connectedk8sShow)
-	http.HandleFunc("/connectedk8sUpdate", aksFunc.Connectedk8sUpdate)
-	http.HandleFunc("/connectedk8sUpgrade", aksFunc.Connectedk8sUpgrade)
+	mux.HandleFunc("/connectedk8sConnect", aksFunc.Connectedk8sConnect)
+	mux.HandleFunc("/connectedk8sDelete", aksFunc.Connectedk8sDelete)
+	mux.HandleFunc("/connectedk8sDisableFeatures", aksFunc.Connectedk8sDisableFeatures)
+	mux.HandleFunc("/connectedk8sEnableFeatures", aksFunc.Connectedk8sEnableFeatures)
+	mux.HandleFunc("/connectedk8sList", aksFunc.Connectedk8sList)
+	mux.HandleFunc("/connectedk8sProxy", aksFunc.Connectedk8sProxy)
+	mux.HandleFunc("/connectedk8sShow", aksFunc.Connectedk8sShow)
+	mux.HandleFunc("/connectedk8sUpdate", aksFunc.Connectedk8sUpdate)
+	mux.HandleFunc("/connectedk8sUpgrade", aksFunc.Connectedk8sUpgrade)
 
 	// etc
-	http.HandleFunc("/aksStart", aksFunc.AksStart)
-	http.HandleFunc("/aksStop", aksFunc.AksStop)
-	http.HandleFunc("/aksRotateCerts", aksFunc.AksRotateCerts)
-	http.HandleFunc("/aksGetOSoptions", aksFunc.AksGetOSoptions)
-	http.HandleFunc("/appUp", aksFunc.AppUp)
-	http.HandleFunc("/browse", aksFunc.Browse)
-	http.HandleFunc("/checkAcr", aksFunc.CheckAcr)
-	http.HandleFunc("/getUpgrades", aksFunc.GetUpgrades)
-	http.HandleFunc("/getVersions", aksFunc.GetVersions)
-	http.HandleFunc("/kanalyze", aksFunc.Kanalyze)
-	http.HandleFunc("/kollect", aksFunc.Kollect)
-	http.HandleFunc("/nodepoolGetUpgrades", aksFunc.NodepoolGetUpgrades)
-	http.HandleFunc("/installCLI", aksFunc.InstallCLI)
-}
+	mux.HandleFunc("/aksStart", aksFunc.AksStart)
+	mux.HandleFunc("/aksStop", aksFunc.AksStop)
+	mux.HandleFunc("/aksRotateCerts", aksFunc.AksRotateCerts)
+	mux.HandleFunc("/aksGetOSoptions", aksFunc.AksGetOSoptions)
+	mux.HandleFunc("/appUp", aksFunc.AppUp)
+	mux.HandleFunc("/browse", aksFunc.Browse)
+	mux.HandleFunc("/checkAcr", aksFunc.CheckAcr)
+	mux.HandleFunc("/getUpgrades", aksFunc.GetUpgrades)
+	mux.HandleFunc("/getVersions", aksFunc.GetVersions)
+	mux.HandleFunc("/kanalyze", aksFunc.Kanalyze)
+	mux.HandleFunc("/kollect", aksFunc.Kollect)
+	mux.HandleFunc("/nodepoolGetUpgrades", aksFunc.NodepoolGetUpgrades)
+	mux.HandleFunc("/installCLI", aksFunc.InstallCLI)
 
-func EKSHandler() {
 	// eks
 
 	// addon
-	http.HandleFunc("/createAddon", eksFunc.CreateAddon)
-	http.HandleFunc("/listAddon", eksFunc.ListAddon)
-	http.HandleFunc("/deleteAddon", eksFunc.DeleteAddon)
-	http.HandleFunc("/describeAddon", eksFunc.DescribeAddon)
-	http.HandleFunc("/updateAddon", eksFunc.UpdateAddon)
-	http.HandleFunc("/describeAddonVersions", eksFunc.DescribeAddonVersions)
+	mux.HandleFunc("/createAddon", eksFunc.CreateAddon)
+	mux.HandleFunc("/listAddon", eksFunc.ListAddon)
+	mux.HandleFunc("/deleteAddon", eksFunc.DeleteAddon)
+	mux.HandleFunc("/describeAddon", eksFunc.DescribeAddon)
+	mux.HandleFunc("/updateAddon", eksFunc.UpdateAddon)
+	mux.HandleFunc("/describeAddonVersions", eksFunc.DescribeAddonVersions)
 
 	// identity-provider
-	http.HandleFunc("/associateIdentityProviderConfig", eksFunc.AssociateIdentityProviderConfig)
-	http.HandleFunc("/disassociateIdentityProviderConfig", eksFunc.DisassociateIdentityProviderConfig)
-	http.HandleFunc("/describeIdentityProviderConfig", eksFunc.DescribeIdentityProviderConfig)
-	http.HandleFunc("/listIdentityProviderConfigs", eksFunc.ListIdentityProviderConfigs)
+	mux.HandleFunc("/associateIdentityProviderConfig", eksFunc.AssociateIdentityProviderConfig)
+	mux.HandleFunc("/disassociateIdentityProviderConfig", eksFunc.DisassociateIdentityProviderConfig)
+	mux.HandleFunc("/describeIdentityProviderConfig", eksFunc.DescribeIdentityProviderConfig)
+	mux.HandleFunc("/listIdentityProviderConfigs", eksFunc.ListIdentityProviderConfigs)
 
 	// tag
-	http.HandleFunc("/tagResource", eksFunc.TagResource)
-	http.HandleFunc("/untagResource", eksFunc.UntagResource)
-	http.HandleFunc("/listTagsForResource", eksFunc.ListTagsForResource)
+	mux.HandleFunc("/tagResource", eksFunc.TagResource)
+	mux.HandleFunc("/untagResource", eksFunc.UntagResource)
+	mux.HandleFunc("/listTagsForResource", eksFunc.ListTagsForResource)
 
 	// update
-	http.HandleFunc("/updateNodegroupConfig", eksFunc.UpdateNodegroupConfig)
-	http.HandleFunc("/updateClusterConfig", eksFunc.UpdateClusterConfig)
+	mux.HandleFunc("/updateNodegroupConfig", eksFunc.UpdateNodegroupConfig)
+	mux.HandleFunc("/updateClusterConfig", eksFunc.UpdateClusterConfig)
 
 	// etc
-	http.HandleFunc("/listUpdate", eksFunc.ListUpdate)
-	http.HandleFunc("/describeUpdate", eksFunc.DescribeUpdate)
-	http.HandleFunc("/associateEncryptionConfig", eksFunc.AssociateEncryptionConfig)
+	mux.HandleFunc("/listUpdate", eksFunc.ListUpdate)
+	mux.HandleFunc("/describeUpdate", eksFunc.DescribeUpdate)
+	mux.HandleFunc("/associateEncryptionConfig", eksFunc.AssociateEncryptionConfig)
+
+	// HCPResource
+	mux.HandleFunc("/resources/deployment", handler.CreateDeploymentHandler).Methods("POST")
+	mux.HandleFunc("/resources/pod", handler.CreatePodHandler).Methods("POST")
+
+	return mux
 }
 
 func main() {
 
 	fmt.Println("start server")
-	AKSHandler()
-	EKSHandler()
-	http.HandleFunc("/resources/deployment", handler.CreateDeploymentHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", handlerRequests())
 }
