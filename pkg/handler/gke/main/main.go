7eaa27a31ab250b648ce56a3e5c2dfbfc8bdbd41
lcw729 <lcw729729@gmail.com>
2022-03-28 18:52:17 +0900

    gcloud container operations describe - go sdk

--- pkg/handler/gke/main/main.go
+++ pkg/handler/gke/main/main.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"cloud.google.com/go/container"
+	container "cloud.google.com/go/container/apiv1"
+	containerpb "google.golang.org/genproto/googleapis/container/v1"
 )
 
 func main() {
-
+	/* container operation list
 	client, err := container.NewClient(context.TODO(), "keti-container")
 	if err != nil {
 		fmt.Println(err)
@@ -24,5 +25,26 @@ func main() {
 	for _, o := range op {
 		fmt.Println(*o)
 	}
+	*/
 
+	// container operation describe
+	// Reference: https://pkg.go.dev/cloud.google.com/go/container/apiv1
+	ctx := context.TODO()
+	c, err := container.NewClusterManagerClient(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer c.Close()
+
+	req := &containerpb.GetOperationRequest{
+		ProjectId:   "keti-container",
+		Zone:        "us-central1-a",
+		OperationId: "operation-1648309236003-34160983",
+		Name:        "operation-1648309236003-34160983",
+	}
+	resp, err := c.GetOperation(ctx, req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(*resp)
 }
