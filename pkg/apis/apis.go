6ac83db13ce8825d5816c6915a166adb28d1898b
root <hth0919@gmail.com>
2023-02-21 12:04:53 +0900

    HC-29-refactor

---
+++ pkg/apis/apis.go
@@ -0,0 +1,81 @@
+package apis
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Response struct {
+	Code int         `json:"code"`
+	Data interface{} `json:"data"`
+}
+
+type Resource interface {
+	Uri() string
+	Get(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response
+	Post(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response
+	Put(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response
+	Delete(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response
+}
+
+type (
+	GetNotSupported    struct{}
+	PostNotSupported   struct{}
+	PutNotSupported    struct{}
+	DeleteNotSupported struct{}
+)
+
+func (GetNotSupported) Get(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response {
+	return Response{405, nil}
+}
+
+func (PostNotSupported) Post(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response {
+	return Response{405, nil}
+}
+
+func (PutNotSupported) Put(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response {
+	return Response{405, nil}
+}
+
+func (DeleteNotSupported) Delete(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) Response {
+	return Response{405, nil}
+}
+
+func abort(rw http.ResponseWriter, statusCode int) {
+	rw.WriteHeader(statusCode)
+}
+
+func HttpResponse(rw http.ResponseWriter, req *http.Request, res Response) {
+	content, err := json.Marshal(res.Data)
+
+	if err != nil {
+		abort(rw, 500)
+	}
+
+	rw.WriteHeader(res.Code)
+	rw.Write(content)
+}
+
+func AddResource(router *httprouter.Router, resource Resource) {
+	fmt.Println("\"" + resource.Uri() + "\" api is registerd")
+
+	router.GET(resource.Uri(), func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		res := resource.Get(rw, r, ps)
+		HttpResponse(rw, r, res)
+	})
+	router.POST(resource.Uri(), func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		res := resource.Post(rw, r, ps)
+		HttpResponse(rw, r, res)
+	})
+	router.PUT(resource.Uri(), func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		res := resource.Put(rw, r, ps)
+		HttpResponse(rw, r, res)
+	})
+	router.DELETE(resource.Uri(), func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		res := resource.Delete(rw, r, ps)
+		HttpResponse(rw, r, res)
+	})
+}
---
+++ pkg/apis/eks/addon/delete.go
@@ -0,0 +1,21 @@
+package addon
+
+import (
+	"hcp-apiserver/pkg/apis"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type DeleteResource struct {
+	apis.PostNotSupported
+	apis.PutNotSupported
+	apis.DeleteNotSupported
+}
+
+func (DeleteResource) Uri() string {
+	return "/eks/addon/delete"
+}
+func (DeleteResource) Get(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) apis.Response {
+	return apis.Response{Code: 200, Data: nil}
+}
