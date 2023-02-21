3bc08b0e5749ae1853dcf89788bb8eebcd40b01c
root <hth0919@gmail.com>
2023-02-21 15:13:46 +0900

    HC-29-refactor

--- pkg/client/aks/aks.go
+++ pkg/client/aks/aks.go
@@ -1,1 +1,34 @@
 package aks
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
+)
+
+func NewAKSClient(k8sclient *kubernetes.Clientset) *armcontainerservice.ManagedClustersClient {
+	config, err := k8sclient.CoreV1().ConfigMaps("public-auth").Get(context.Background(), "aks-auth", metav1.GetOptions{})
+	if err != nil {
+		klog.Errorln(err.Error())
+	}
+
+	tanantID := config.Data["tenant-id"]
+	clientID := config.Data["client-id"]
+	clientSecret := config.Data["client-secret"]
+	subscriptionID := config.Data["ccfc0c6c-d3c6-4de2-9a6c-c09ca498ff73"]
+
+	credention, err := azidentity.NewClientSecretCredential(tanantID, clientID, clientSecret, new(azidentity.ClientSecretCredentialOptions))
+	if err != nil {
+		klog.Errorln(err.Error())
+	}
+	client, err := armcontainerservice.NewManagedClustersClient(subscriptionID, credention, new(policy.ClientOptions))
+	if err != nil {
+		klog.Errorln(err.Error())
+	}
+	return client
+}
--- pkg/client/kubernetes/kubernetes.go
+++ pkg/client/kubernetes/kubernetes.go
@@ -1,1 +1,65 @@
 package kubernetes
+
+import (
+	"hcp-apiserver/pkg/client/aks"
+	"hcp-apiserver/pkg/client/eks"
+	"os"
+	"path/filepath"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
+
+type HCPClient struct {
+	KubeClient *kubernetes.Clientset
+	EKSClient  *session.Session
+	AKSClient  *armcontainerservice.ManagedClustersClient
+	GKEClient  string
+	NKSClient  string
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // 윈도우에서는 USERPROFILE을 사용
+}
+
+func InitHCPClient() *HCPClient {
+	var kubeconfig string
+	config := new(rest.Config)
+	var err error
+
+	hcpClient := &HCPClient{}
+
+	if home := homeDir(); home != "" {
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	} else {
+		kubeconfig = ""
+	}
+
+	if len(kubeconfig) == 0 {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			klog.Errorln(err.Error())
+		}
+	} else {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			klog.Errorln(err.Error())
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Errorln(err.Error())
+	}
+	hcpClient.KubeClient = clientset
+	hcpClient.EKSClient = eks.NewEKSClient(clientset)
+	hcpClient.AKSClient = aks.NewAKSClient(clientset)
+	return nil
+}
