b1008a360c93a531c8a90c070f0d4fe78704cafd
root <hth0919@gmail.com>
2023-04-05 18:00:00 +0900

    HC-29-refactor

---
+++ pkg/apis/eks/addon/create.go
@@ -0,0 +1,21 @@
+package addon
+
+import (
+	"hcp-apiserver/pkg/apis"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type CreateReaource struct {
+	apis.PostNotSupported
+	apis.PutNotSupported
+	apis.DeleteNotSupported
+}
+
+func (CreateReaource) Uri() string {
+	return "/eks/addon/create"
+}
+func (CreateReaource) Get(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) apis.Response {
+	return apis.Response{Code: 200, Data: nil}
+}
--- pkg/handler/backup/struct.go
+++
@@ -1,6 +0,0 @@
-package backup
-
-type ClusterInfo struct {
-	PlatformName string
-	ClusterName  string
-}
