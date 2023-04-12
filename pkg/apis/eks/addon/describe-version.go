e7a8d7b5b90f5bd6d15b331f87fe5e5edc37150e
root <hth0919@gmail.com>
2023-04-12 18:00:00 +0900

    HC-29-refactor

--- pkg/apis/eks/addon/describe-version.go
+++ pkg/apis/eks/addon/describe-version.go
@@ -1,1 +1,21 @@
 package addon
+
+import (
+	"hcp-apiserver/pkg/apis"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type DescribeVersionResource struct {
+	apis.PostNotSupported
+	apis.PutNotSupported
+	apis.DeleteNotSupported
+}
+
+func (DescribeVersionResource) Uri() string {
+	return "/eks/addon/describe-version"
+}
+func (DescribeVersionResource) Get(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) apis.Response {
+	return apis.Response{Code: 200, Data: nil}
+}
--- pkg/apis/eks/addon/update.go
+++ pkg/apis/eks/addon/update.go
@@ -1,1 +1,21 @@
 package addon
+
+import (
+	"hcp-apiserver/pkg/apis"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type UpdateResource struct {
+	apis.PostNotSupported
+	apis.PutNotSupported
+	apis.DeleteNotSupported
+}
+
+func (UpdateResource) Uri() string {
+	return "/eks/addon/list"
+}
+func (UpdateResource) Get(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) apis.Response {
+	return apis.Response{Code: 200, Data: nil}
+}
